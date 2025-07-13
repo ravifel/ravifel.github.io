d8481d5b6be88589ab658e2fc0ed98dc8979f2f5
ravifel <ravifelipee@gmail.com>
2025-07-13 20:39:18 -0300

    New React portfólio

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState, createContext } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
+import RoutesApp from './routes';
+import './index.css';
+
+export const ThemeContext = createContext(); // Theme Context
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleTheme = () => setDarkMode((prev) => !prev);
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleTheme }}>
+      <div className={darkMode ? 'dark-theme' : 'light-theme'}>
+        <BrowserRouter>
+          <Header />
+          <div className="container mt-4">
+            <RoutesApp />
+          </div>
+        </BrowserRouter>
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
